5c6872e41357be271fd8812e40615e157f157073
liugogal <259394345@qq.com>
2018-05-31 17:27:21 +0800

    update: BaiduOcr.js

--- www/BaiduOcr.js
+++ www/BaiduOcr.js
@@ -11,90 +11,80 @@
 var exec = cordova.require("cordova/exec");
 
 
-/**
- * Constructor.
- *
- * @returns {BaiduOcr}
- */
-function BaiduOcr() {
+var BaiduOcr = {
+    /**
+     * 初始化ocr
+     * @param successCallback
+     * @param errorCallback
+     */
+    init: function (successCallback, errorCallback) {
 
 
-}
+        if (errorCallback == null) {
+            errorCallback = function () {
+            };
+        }
 
-/**
- * 初始化ocr
- * @param successCallback
- * @param errorCallback
- */
-BaiduOcr.prototype.init = function (successCallback, errorCallback) {
+        if (typeof errorCallback !== "function") {
+            console.log("BaiduOcr.init failure: failure parameter not a function");
+            return;
+        }
 
+        if (typeof successCallback !== "function") {
+            console.log("BaiduOcr.init failure: success callback parameter must be a function");
+            return;
+        }
 
-    if (errorCallback == null) {
-        errorCallback = function () {
-        };
+        exec(successCallback, errorCallback, 'BaiduOcr', 'init');
+    },
+    /**
+     * 扫描身份证
+     * @param successCallback
+     * @param errorCallback
+     * @param params 其中的contentType取值： IDCardFront(正面),IDCardBack(反面)
+     */
+    scanId: function (successCallback, errorCallback, params) {
+
+        if (errorCallback == null) {
+            errorCallback = function () {
+            };
+        }
+
+        if (typeof errorCallback !== "function") {
+            console.log("BaiduOcr.scanId failure: failure parameter not a function");
+            return;
+        }
+
+        if (typeof successCallback !== "function") {
+            console.log("BaiduOcr.scanId failure: success callback parameter must be a function");
+            return;
+        }
+
+        exec(successCallback, errorCallback, 'BaiduOcr', 'scanId', [params]);
+    },
+    /**
+     * 销毁
+     * @param successCallback
+     * @param errorCallback
+     */
+    destroy: function (successCallback, errorCallback) {
+        if (errorCallback == null) {
+            errorCallback = function () {
+            };
+        }
+
+        if (typeof errorCallback !== "function") {
+            console.log("BaiduOcr.destroy failure: failure parameter not a function");
+            return;
+        }
+
+        if (typeof successCallback !== "function") {
+            console.log("BaiduOcr.destroy failure: success callback parameter must be a function");
+            return;
+        }
+
+        exec(successCallback, errorCallback, 'BaiduOcr', 'destroy');
     }
-
-    if (typeof errorCallback !== "function") {
-        console.log("BaiduOcr.init failure: failure parameter not a function");
-        return;
-    }
-
-    if (typeof successCallback !== "function") {
-        console.log("BaiduOcr.init failure: success callback parameter must be a function");
-        return;
-    }
-
-    exec(successCallback, errorCallback, 'BaiduOcr', 'init');
-};
-
-/**
- * 扫描身份证
- * @param successCallback
- * @param errorCallback
- * @param params 其中的contentType取值： IDCardFront(正面),IDCardBack(反面)
- */
-BaiduOcr.prototype.scanId = function (successCallback, errorCallback, params) {
-
-    if (errorCallback == null) {
-        errorCallback = function () {
-        };
-    }
-
-    if (typeof errorCallback !== "function") {
-        console.log("BaiduOcr.scanId failure: failure parameter not a function");
-        return;
-    }
-
-    if (typeof successCallback !== "function") {
-        console.log("BaiduOcr.scanId failure: success callback parameter must be a function");
-        return;
-    }
-
-    exec(successCallback, errorCallback, 'BaiduOcr', 'scanId', [params]);
-};
-
-/**
- * 销毁
- * @param successCallback
- * @param errorCallback
- */
-BaiduOcr.prototype.destroy = function (successCallback, errorCallback) {
-    if (errorCallback == null) {
-        errorCallback = function () {
-        };
-    }
-
-    if (typeof errorCallback !== "function") {
-        console.log("BaiduOcr.destroy failure: failure parameter not a function");
-        return;
-    }
-
-    if (typeof successCallback !== "function") {
-        console.log("BaiduOcr.destroy failure: success callback parameter must be a function");
-        return;
-    }
-
-    exec(successCallback, errorCallback, 'BaiduOcr', 'destroy');
 };
 
 
