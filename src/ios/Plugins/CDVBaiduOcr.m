edad9b372007579a52288ee4d760ad5e7629ed45
liugogal <259394345@qq.com>
2018-06-04 17:30:32 +0800

    update: add ios beta

--- src/ios/Plugins/CDVBaiduOcr.m
+++ src/ios/Plugins/CDVBaiduOcr.m
@@ -10,10 +10,32 @@
 #import <objc/runtime.h>
 #import <AipOcrSdk/AipOcrSdk.h>
 
+BOOL hasGotToken = NO;
+
 @implementation CDVBaiduOcr
 
 - (void)init:(CDVInvokedUrlCommand *)command {
-    [[AipOcrService shardService] authWithAK:@"AK" andSK:@"SK"];
+    //     授权方法1：在此处填写App的Api Key/Secret Key
+//    [[AipOcrService shardService] authWithAK:@"AK" andSK:@"SK"];
+
+
+    // 授权方法2（更安全）： 下载授权文件，添加至资源
+    NSString *licenseFile = [[NSBundle mainBundle] pathForResource:@"aip-2" ofType:@"license"];
+    NSData *licenseFileData = [NSData dataWithContentsOfFile:licenseFile];
+    if(!licenseFileData) {
+        [[[UIAlertView alloc] initWithTitle:@"授权失败" message:@"授权文件不存在" delegate:nil cancelButtonTitle:@"确定" otherButtonTitles:nil] show];
+    }
+    [[AipOcrService shardService] authWithLicenseFileData:licenseFileData];
+
+    //获取token回调
+    [[AipOcrService shardService] getTokenSuccessHandler:^(NSString *token) {
+        NSLog(@"token: %@",token);
+        hasGotToken = YES;
+    } failHandler:^(NSError *error) {
+        NSLog(@"error: %@",error.domain);
+        hasGotToken = NO;
+    }];
+
 }
 
 - (void)scanId:(CDVInvokedUrlCommand *)command {
